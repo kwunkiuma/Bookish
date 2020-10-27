34be88f16440740d7caa3ffcdee9c5309b064a2c
FraMa <franky.ma24@gmail.com>
2020-10-27 15:25:02 +0000

    Changed tables structure

--- Bookish.DataAccess/InsertAccounts.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Accounts (UserID, Username)
-VALUES
-	(1, 'Name1'),
-	(2, 'Name2'),
-	(3, 'Name3'),
-	(4, 'Name4')
-GO
--- Bookish.Web/Startup.cs
+++ Bookish.Web/Startup.cs
@@ -30,8 +30,7 @@ namespace Bookish.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer("Server = localhost; Database = Bookish; Trusted_Connection = True;"));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
